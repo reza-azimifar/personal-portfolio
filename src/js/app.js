af1ce69e2a7c1c9db71026be78f3ddd014913c91
Reza Azimifar <azimifar.reza@gmail.com>
2024-06-24 03:05:30 +0330

    sidebar creation and styling part 1

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,13 @@ window.addEventListener("scroll", () => {
         navbar.classList.remove("navbar-fixed");
     }
 });
+
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+navBtn.addEventListener("click", () => {
+    sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("show-sidebar");
+});
